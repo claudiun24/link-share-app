5a7f5ae1003cb53e759584030d9a993ef6a495ca
claudiun24 <claudiun@gmail.com>
2024-12-05 21:11:08 +0200

    Implemented Text component

---
+++ src/common/components/text/index.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import Container from "./index.styled";
+
+const Text = (props) => {
+  return <Container>{props.children}</Container>;
+};
+
+Text.propTypes = {
+  children: PropTypes.element,
+  size: PropTypes.oneOf(["sm", "md"])
+};
+export default Text;
