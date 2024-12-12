80890e977d63d40f5b447789d77c7f283f036293
claudiun24 <claudiun@gmail.com>
2024-12-12 12:29:21 +0200

    Corrected import from the root index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { AuthLoginPage, AuthRegisterPage } from "./pages";
 import { AuthLoginPage, AuthRegisterPage, LinksPage } from "./pages";
 
 const router = createBrowserRouter([
