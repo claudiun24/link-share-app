2cb454bc102fd3144058c32710cb25a056856b59
claudiun24 <claudiun@gmail.com>
2024-12-20 11:55:35 +0200

    Implemented link api endpoints

--- src/api/entities/link.entity.js
+++ src/api/entities/link.entity.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../index.config";
 const entity = {
   create: async (payload) => {
     try {
-      const response = await axiosInstance.post("./links.json", {
+      const response = await axiosInstance.post("/links.json", {
         platform: payload.platform,
         url: payload.url,
         userId: payload.userId
@@ -11,41 +11,48 @@ const entity = {
       return response.data;
     } catch (error) {
       console.log(
-        "[API-ERROR]: Entity: .Link() | Method: .create() | Message - ",
+        "[API-ERROR]: Entity: Link | Method: .create() | Message - ",
         error.toString()
       );
     }
   },
   updateById: async (id, payload) => {
     try {
-      const response = await axiosInstance.post("./links.json", {
+      const response = await axiosInstance.put(`/links/${id}.json`, {
         platform: payload.platform,
         url: payload.url
       });
       return response.data;
     } catch (error) {
       console.log(
-        "[API-ERROR]: Entity: .Link() | Method: .updateById() | Message - ",
+        "[API-ERROR]: Entity: Link | Method: .updateById() | Message - ",
         error.toString()
       );
     }
   },
   readAllByUserId: async (userId) => {
     try {
-      const response = await axiosInstance.get(`./links.json`, {});
+      const response = await axiosInstance.get(`/links.json`, {
+        params: {
+          orderBy: '"userId"',
+          equalTo: `"${userId}"`
+        }
+      });
+      return response.data;
     } catch (error) {
       console.log(
-        "[API-ERROR]: Entity: .Link() | Method: .readAll() | Message - ",
+        "[API-ERROR]: Entity: Link | Method: .readAllByUserId() | Message - ",
         error.toString()
       );
     }
   },
-  deleteById: async () => {
+  deleteById: async (id) => {
     try {
-      //our code
+      const response = await axiosInstance.delete(`/links/${id}.json`);
+      return response.data;
     } catch (error) {
       console.log(
-        "[API-ERROR]: Entity: .Link() | Method: .delete() | Message - ",
+        "[API-ERROR]: Entity: Link | Method: .deleteById() | Message - ",
         error.toString()
       );
     }
